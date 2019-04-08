b3e0020dac914dc36cf221aa58edbce3ff96f34c
Marko Arandjelovic <marko977x@gmail.com>
2019-04-08 14:55:10 +0200

    Add project files

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    devtool: "inline-source-map",
+    mode: "development",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "dist",
+        filename: "bundle.js"
+    }
+};
